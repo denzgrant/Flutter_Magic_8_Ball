2801ce6d2ba8902f261a2b7f7417f16d4ec4569e
Denzell Grant <denzell.grant@embryhealth.com>
2021-03-03 08:05:26 -0700

    inital commit

---
+++ android/app/src/main/kotlin/com/example/Magic_8_Ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Magic_8_Ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(),
+    );
+  }
+}
